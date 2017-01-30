058d2acbec912120309341af78ce2c9f98a62345
Luke Barry <luke-barry@hotmail.com>
2017-01-30 20:12:50 +0000

    master file

--- form/views.py
+++ form/views.py
@@ -3,12 +3,12 @@ from . import app
 from .database import session, Entry
 from flask import request, redirect, url_for
 
-@app.route("/")
+@app.route("/view")
 def entries():
     entries = session.query(Entry)
     entries = entries.order_by(Entry.datetime.desc())
     entries = entries.all()
-    return render_template("base.html", entries=entries)
+    return render_template("entries.html", entries=entries)
 
 @app.route("/", methods=["GET"])
 def add_entry_get():
@@ -31,8 +31,28 @@ def add_entry_post():
     session.add(entry)
     session.commit()
     return redirect("/")
+
+
+@app.route("/view", methods=["GET"])
+def view():
+    return render_template("entries.html")
     
-  
+
+@app.route("/view", methods=["POST"])
+def view_post():
+    entry = Entry(
+        student_id=request.form["student_id"],
+        student_name=request.form["student_name"],
+        parent_name=request.form["parent_name"],
+        site_location=request.form["site_location"],
+        phone_number=request.form["phone_number"],
+        alt_number=request.form["alt_number"],
+        notes=request.form["notes"],
+        # datetime=request.form["datetime"]      
+    )
+    session.add(entry)
+    session.commit()
+    return redirect("/view")  
 
   
   
