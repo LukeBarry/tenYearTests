950595548eb5dfc1bf6ec5dae0300dcaae5929ea
Luke Barry <luke-barry@hotmail.com>
2017-01-31 23:19:59 +0000

    change config

--- form/config.py
+++ form/config.py
@@ -1,5 +1,5 @@
 import os
 class DevelopmentConfig(object):
-    # SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/form"
-    SQLAlCHEMY_DATABASE_URI = "postgres://pbnybuchhwygrq:baad39050d87267c3caaf9cc02ce184264d3878a7c77a032f12d3b29f36b6e92@ec2-23-21-80-230.compute-1.amazonaws.com:5432/d5i6t9lpps2s0d"
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/form"
+    # SQLAlCHEMY_DATABASE_URI = "postgres://pbnybuchhwygrq:baad39050d87267c3caaf9cc02ce184264d3878a7c77a032f12d3b29f36b6e92@ec2-23-21-80-230.compute-1.amazonaws.com:5432/d5i6t9lpps2s0d"
     DEBUG = True
