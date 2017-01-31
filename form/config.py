0268276c06ef9e5fc2e9219ffa21a92819ca9b2d
Luke Barry <luke-barry@hotmail.com>
2017-01-31 18:27:12 +0000

    added heroku psgreql URI

--- form/config.py
+++ form/config.py
@@ -1,4 +1,5 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/form"
+    # SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/form"
+    SQLAlCHEMY_DATABASE_URI = "postgres://nscouxccjfsvap:077a808da48fb81d6a2f080a9cf60ba0dbba36f5b4dfddf69eea321ccf6b3a2e@ec2-23-21-169-238.compute-1.amazonaws.com:5432/d6dr0r0q3j3ubm"
     DEBUG = True
